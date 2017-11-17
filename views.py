936e2df6362b576bf194d215e75bc769a4b08e5b
Anjali Jain <aj2966@gmail.com>
2017-11-17 15:28:23 +0530

    Update views.py

--- views.py
+++ views.py
@@ -4,4 +4,4 @@ def index(request):
     return HttpResponse("<h1>This will be a list of all albums</h1>")
 
 def detail(request, album_id):
-    return HttpResponse("<h2> Details for Album id: " + str(album_id) + "</h2")
+    return HttpResponse("<h2> Details for Album id: " + str(album_id) + "</h2>")
